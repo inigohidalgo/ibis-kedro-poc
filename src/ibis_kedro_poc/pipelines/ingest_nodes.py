16c907423312994a03342eea2300fed61ed1beab
Iñigo Hidalgo Rey <inigohrey@gmail.com>
2023-06-17 14:52:24 +0200

    feat: add node to create db using `IbisDataSet.save`

--- src/ibis_kedro_poc/pipelines/ingest_nodes.py
+++ src/ibis_kedro_poc/pipelines/ingest_nodes.py
@@ -1,6 +1,13 @@
 import ibis
 import pandas as pd
 
+def create_dataframe() -> pd.DataFrame:
+    test_data = pd.DataFrame({
+    "date": pd.date_range("2022-01-01", "2022-01-10"),
+    "group": ["A","A","A","A","A","B","B","B","B","B",],
+    "value":[1,2,3,4,5,6,7,8,9,10]
+})
+    return test_data
 
 def filter_dates(data: ibis.table, start_date: str, end_date: str) -> ibis.table:
     return data.filter([data.date >= start_date, data.date <= end_date])
--- src/ibis_kedro_poc/pipelines/ingest_pipeline.py
+++ src/ibis_kedro_poc/pipelines/ingest_pipeline.py
@@ -1,9 +1,14 @@
 from kedro.pipeline import node, Pipeline
 
-from .ingest_nodes import filter_dates, ibis_table_to_df
+from .ingest_nodes import create_dataframe, filter_dates, ibis_table_to_df
 
 ingest_pipeline = Pipeline(
     [
+        node(
+            func=create_dataframe,
+            inputs=None,
+            outputs="raw_table",
+        ),
         node(
             func=filter_dates,
             inputs=["raw_table", "params:date_from", "params:date_to"],
